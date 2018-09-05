9fa3c6f66137d539394a6cefa9251c5efdc07ad4
Richard Cheney <richard.cheney@microsoft.com>
2018-09-05 09:25:14 +0100

    More error trapping

--- workshops/terraform/installLatestTerraform.sh
+++ workshops/terraform/installLatestTerraform.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+error()
+{
+  if [[ -n "$@" ]]
+  then
+    tput setaf 1
+    echo "ERROR: $@" >&2
+    tpu sgr0
+  fi
+
+  [[ -n "$zip" ]] && [[ -f /tmp/$zip ]] && rm /tmp/$zip
+
+  exit 1
+}
+
 # Install zip if not there
 [[ ! -x /usr/bin/zip ]] && sudo apt-get --assume-yes -qq install zip
+[[ ! -x /usr/bin/zip ]] && error "Install package \"zip\" and rerun"
 
 # Install jq if not there
 [[ ! -x /usr/bin/jq ]] && sudo apt-get --assume-yes -qq install jq
+[[ ! -x /usr/bin/jq ]] && error "Install package \"jq\" and rerun"
 
 # Determine latest file using the API
 latest=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')
@@ -17,9 +33,7 @@ curl --silent --output /tmp/$zip $dir/$zip
 
 if [[ "$(cd /tmp; sha256sum $zip)" != "$(curl -s $dir/terraform_${latest}_SHA256SUMS | grep $zip)" ]]
 then
-  echo "ERROR: Downloaded zip does not match SHA256 checksum value - removing" >&2
-  rm /tmp/$zip
-  exit 1
+  error "Downloaded zip does not match SHA256 checksum value - removing"
 else
   echo "Extracting terraform executable ..." >&2
   unzip -oq /tmp/$zip terraform -d /tmp && rm /tmp/$zip
